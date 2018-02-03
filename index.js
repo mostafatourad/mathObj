38a41f4a8ebc0c6e8c8baca9dacfe7f70bf44365
Mostafa Salama <mostafalearning2016@gmail.com>
2018-02-03 18:53:57 +0200

    fixing arugment error in min function

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ math.max = function (...arg) {
     return Math.max(...arg) ;
 }
 math.min = function (...arg) {
-    return Math.min(arg) ;
+    return Math.min(...arg) ;
 }
 console.log(math.max(12,2,3,4,5))
