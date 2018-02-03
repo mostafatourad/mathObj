010e330b9650b1b5e88bf4f42332fb02bc9bb58a
Mostafa Salama <mostafalearning2016@gmail.com>
2018-02-03 18:55:08 +0200

    adding average function

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ math.max = function (...arg) {
 math.min = function (...arg) {
     return Math.min(...arg) ;
 }
+math.avg = function (...args) {
+    let total = 0 ;
+    for(let arg of args ){
+        total+=arg ;
+    }
+    return total/args.length ; 
+}
 console.log(math.max(12,2,3,4,5))
